1da543124835e0a1b7a6e39bbbdc1d62073ecd6c
AGannon22 <gannonalyssa@gmail.com>
2026-06-20 08:34:52 -0400

    added my work experience to the list

--- app/__init__.py
+++ app/__init__.py
@@ -18,4 +18,9 @@ def map():
     return render_template('map.html', title="Map", url=os.getenv("URL"))
 @app.route('/experience')
 def experience():
+    work_experience = [
+        {"name": "Intern @ Motorboat Mechanics", "time": "May 2026-Present", "img": "img1.jpg", "description": "I currently work as an intern at Motorboat Mechanics, I help with various tasks such as automation, setting up appointments for customers, and building a website to track company data."},
+        {"name": "Intern @ Motorboat Mechanics", "time": "Dec 2024-Oct 2025", "img": "img2.jpg", "description": "As a restaurant team member at Tim Hortons, I would do a variety of tasks such as taking orders, preparing food, and making coffee and other drinks."},
+        {"name": "Instructor @ iD Tech Columbia", "time": "Jun 2025-Aug 2025", "img": "img3.jpg", "description": "I was an instructor at iD Tech at Columbia University, where I taught classes on Machine Learning, Scratch, and Minecraft Modding to students ages 6-17."},
+    ]
     return render_template('hobbies.html', title="Experience", url=os.getenv("URL"))
