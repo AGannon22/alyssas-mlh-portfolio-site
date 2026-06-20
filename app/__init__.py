64169eec42447e1c35ca80f4c1685d37679573af
AGannon22 <gannonalyssa@gmail.com>
2026-06-20 08:54:39 -0400

    updated hobbie dicts with time and correct image paths

--- app/__init__.py
+++ app/__init__.py
@@ -13,11 +13,11 @@ def index():
 @app.route('/hobbies')
 def hobbies():
     hobbies = [
-        {"name": "Cooking", "img": "img1.jpg","description": "I only recently started cooking since I started college, but I really enjoy finding new recipes to try out! My favorite foods to make right now are carbonara and porkchops with baked potatoes!"},
-        {"name": "Baking", "img": "img2.jpg","description": "I've been baking for longer than I can remember. My grandmother taught me how to make chocolate chip cookies when I was a kid and I've been baking ever since. My favorite thing to bake are my chocolate chip and sugar cookie brookies!"},
-        {"name": "Gaming", "img": "img3.jpg","description": "My gaming interests started in second grade with Pokemon and Tomodachi Life on the 3DS. I still play both to this day, but I've also gotten into Valorant, Stardew Valley, Siege, and more!"},
-        {"name": "Drawing", "img": "img4.jpg","description": "I've been drawing literally forever. I was going to be a Graphic Designer before I switched to Comp Sci. I was active in my Art Honors Society and still draw and make art on occasion!"},
-        {"name": "Skiing", "img": "img5.jpg","description": "Skiing is life. There is no better way to destroy my calfs and knees. I started skiing at 13, and it's been a tradition in my friend group to go skiing every winter break for the past 4 years! Shoutout Killington!"},
+        {"name": "Cooking", "time": "3 years", "img": "app/static/img/hobby_imgs/img1.jpg", "description": "I only recently started cooking since I started college, but I really enjoy finding new recipes to try out! My favorite foods to make right now are carbonara and porkchops with baked potatoes!"},
+        {"name": "Baking", "time": "10 years", "img": "app/static/img/hobby_imgs/img2.jpg","description": "I've been baking for longer than I can remember. My grandmother taught me how to make chocolate chip cookies when I was a kid and I've been baking ever since. My favorite thing to bake are my chocolate chip and sugar cookie brookies!"},
+        {"name": "Gaming", "time": "7 years", "img": "app/static/img/hobby_imgs/img3.jpg","description": "My gaming interests started in second grade with Pokemon and Tomodachi Life on the 3DS. I still play both to this day, but I've also gotten into Valorant, Stardew Valley, Siege, and more!"},
+        {"name": "Drawing", "time": "12 years", "img": "app/static/img/hobby_imgs/img4.jpg","description": "I've been drawing literally forever. I was going to be a Graphic Designer before I switched to Comp Sci. I was active in my Art Honors Society and still draw and make art on occasion!"},
+        {"name": "Skiing", "time": "5 years", "img": "app/static/img/hobby_imgs/img5.jpg","description": "Skiing is life. There is no better way to destroy my calfs and knees. I started skiing at 13, and it's been a tradition in my friend group to go skiing every winter break for the past 4 years! Shoutout Killington!"},
     ]
     return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
 
