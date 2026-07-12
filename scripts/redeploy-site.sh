a2e9141544f7915317ec88a7f54967affde1c055
AGannon22 <gannonalyssa@gmail.com>
2026-07-12 01:11:21 -0400

    new redeploy-site.sh changes for task 6

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-tmux kill-server
+set -e
 cd ~
 cd portfolio-website/alyssas-mlh-portfolio-site
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 deactivate
-tmux new-session -d -s "portfolio-server" \ "cd ~/portfolio-website/alyssas-mlh-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0 --port=5000"
+sudo systemctl restart myportfolio
